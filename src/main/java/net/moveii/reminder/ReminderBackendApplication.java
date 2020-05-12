ae961189bba0c104252e3bd3ed1173edf807d7df
moveii <patrick.berger256@gmail.com>
2020-05-12 16:36:19 +0200

    Project Setup

---
+++ src/main/java/net/moveii/reminder/ReminderBackendApplication.java
@@ -0,0 +1,13 @@
+package net.moveii.reminder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReminderBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReminderBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/net/moveii/reminder/ReminderBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package net.moveii.reminder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReminderBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
